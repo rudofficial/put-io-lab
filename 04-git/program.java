30df01f9bd4d1d6a8b34e5410516baa0e39aef4f
unknown <rudnevoleg02@gmail.com>
2023-11-04 01:36:37 +0300

    modified:   program.java
    modified:   program.java

--- 04-git/program.java
+++ 04-git/program.java
@@ -1,5 +1,9 @@
+//komentarz
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello-!");
+        System.out.println("World!.");
     }
 }
