235555e23e01de9b1575bbf7ab6db10d3f0569f1
unknown <rudnevoleg02@gmail.com>
2023-11-05 01:01:37 +0300

    program.java i README.md

--- 04-git/program.java
+++ 04-git/program.java
@@ -3,7 +3,5 @@ public class Program {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        System.out.println("Hello-!");
-        System.out.println("World!.");
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # put-io-lab
 # Aleh Rudneu
-<<<<<<< HEAD
-
-
-# 02.11.2023
->>>>>>> eb7b783461c54e3d2ec3814142c6cdd56eb6717f
+# 03.11.2023
